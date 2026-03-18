1a0e7e934f13eb3ec00ce45dd9d6a8c6dfc6eb48
Dirk Jan <dirkjan.schep@gmail.com>
2026-03-18 20:42:51 +0100

    Fix typo in DucoFlowLevelValve sensor class names

--- esphome/components/duco/sensor/sensor.h
+++ esphome/components/duco/sensor/sensor.h
@@ -53,7 +53,7 @@ class DucoTemperatureSensor : public DucoDevice, public PollingComponent, public
   uint8_t address_;
 };
 
-class DucoFLowLevelValve1Sensor : public DucoDevice, public PollingComponent, public sensor::Sensor {
+class DucoFlowLevelValve1Sensor : public DucoDevice, public PollingComponent, public sensor::Sensor {
  public:
   void setup() override;
   void update() override;
@@ -68,7 +68,7 @@ class DucoFLowLevelValve1Sensor : public DucoDevice, public PollingComponent, pu
   uint8_t address_;
 };
 
-class DucoFLowLevelValve2Sensor : public DucoDevice, public PollingComponent, public sensor::Sensor {
+class DucoFlowLevelValve2Sensor : public DucoDevice, public PollingComponent, public sensor::Sensor {
  public:
   void setup() override;
   void update() override;
