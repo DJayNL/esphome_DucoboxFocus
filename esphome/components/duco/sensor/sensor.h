99cfce026c699dfb7829dc8fbfe8233a92b9f314
Dirk Jan <dirkjan.schep@gmail.com>
2026-03-18 11:07:11 +0100

    Rename and add DucoValveFlowLevelSensor classes

--- esphome/components/duco/sensor/sensor.h
+++ esphome/components/duco/sensor/sensor.h
@@ -53,7 +53,7 @@ class DucoTemperatureSensor : public DucoDevice, public PollingComponent, public
   uint8_t address_;
 };
 
-class DucoValveFlowLevelSensor : public DucoDevice, public PollingComponent, public sensor::Sensor {
+class DucoValveFlowLevelSensor1 : public DucoDevice, public PollingComponent, public sensor::Sensor {
  public:
   void setup() override;
   void update() override;
@@ -68,6 +68,21 @@ class DucoValveFlowLevelSensor : public DucoDevice, public PollingComponent, pub
   uint8_t address_;
 };
 
+class DucoValveFlowLevelSensor2 : public DucoDevice, public PollingComponent, public sensor::Sensor {
+ public:
+  void setup() override;
+  void update() override;
+
+  float get_setup_priority() const override;
+
+  void receive_response(const DucoMessage &message) override;
+
+  void set_address(uint8_t address);
+
+ protected:
+  uint8_t address_;
+
+};
 class DucoBoxTemperatureSensor : public DucoDevice, public PollingComponent, public sensor::Sensor {
  public:
   void setup() override;
