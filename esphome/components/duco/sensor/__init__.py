aa3d053d0d8ceea8f69cd968b1d3451e5e9afda3
Dirk Jan <dirkjan.schep@gmail.com>
2026-03-21 22:50:45 +0100

    Remove DucoScannerSensor and scanner configuration
    Removed DucoScannerSensor and related scanner configuration from the schema.

--- esphome/components/duco/sensor/__init__.py
+++ esphome/components/duco/sensor/__init__.py
@@ -72,11 +72,6 @@ DucoFlowLevelValve2Sensor = duco_ns.class_(
 DucoStateTimeRemainingSensor = duco_ns.class_(
     "DucoStateTimeRemainingSensor", cg.PollingComponent, sensor.Sensor
 )
-DucoScannerSensor = duco_ns.class_(
-    "DucoScannerSensor",
-    cg.PollingComponent,
-    sensor.Sensor,
-)
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -94,14 +89,7 @@ CONFIG_SCHEMA = cv.Schema(
                 }
             )
             .extend(cv.polling_component_schema("60s"))
-        ),
-        cv.Optional("scanner"): sensor.sensor_schema().extend(
-            {
-                cv.Required(CONF_ADDRESS): cv.hex_uint8_t,
-            }
-            )
-            .extend(cv.polling_component_schema("60s")),
-        
+        ),        
         cv.Optional(CONF_TEMPERATURE): cv.ensure_list(
             sensor.sensor_schema(
                 unit_of_measurement=UNIT_CELSIUS,
@@ -273,14 +261,6 @@ async def to_code(config):
             cg.add(sensvar.set_parent(parent))
             cg.add(sensvar.set_address(flow_level_valve2_config[CONF_ADDRESS]))
 
-    if "scanner" in config:
-        conf = config["scanner"]
-        sens = await sensor.new_sensor(conf)
-
-        cg.add(sens.set_address(conf[CONF_ADDRESS]))
-        await cg.register_component(sens, conf)
-        await duco.register_device(sens)
-            
     if CONF_FILTER_REMAINING in config:
         filter_remaining_config = config[CONF_FILTER_REMAINING]
         sensvar = cg.new_Pvariable(filter_remaining_config[CONF_ID])
