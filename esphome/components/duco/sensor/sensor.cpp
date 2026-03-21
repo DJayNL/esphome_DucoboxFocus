8b5a489f7a27e144edf7b5b9bbb10077c5cff6a5
Dirk Jan <dirkjan.schep@gmail.com>
2026-03-22 00:33:42 +0100

    Fix data order in DucoFlowLevelValve1Sensor update

--- esphome/components/duco/sensor/sensor.cpp
+++ esphome/components/duco/sensor/sensor.cpp
@@ -96,7 +96,7 @@ void DucoFlowLevelValve1Sensor::setup() {}
 void DucoFlowLevelValve1Sensor::update() {
   DucoMessage message;
   message.function = 0x10;
-  message.data = {0x01, address_, 0x00, 0x4A, 0x04};
+  message.data = {0x01, 0x04, 0x00, address_, 0x04};
   this->parent_->send(message, this);
 }
 
